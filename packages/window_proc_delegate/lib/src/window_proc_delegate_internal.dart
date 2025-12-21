57abaff584540270ef13b807424ce72d7de8c0ab
boyan01 <17426470+boyan01@users.noreply.github.com>
2025-12-21 20:56:38 +0800

    fix: remove unused import and correct native callback reference

--- packages/window_proc_delegate/lib/src/window_proc_delegate_internal.dart
+++ packages/window_proc_delegate/lib/src/window_proc_delegate_internal.dart
@@ -1,7 +1,6 @@
 import 'dart:ffi' as ffi;
 import 'dart:io';
 import 'package:flutter/foundation.dart';
-import 'package:flutter/services.dart';
 import 'windows_message.dart';
 import '../window_proc_delegate.dart';
 
@@ -71,7 +70,7 @@ void initialize(
   // Get the engine ID and register the native callback
   try {
     final engineId = PlatformDispatcher.instance.engineId!;
-    setCallback(engineId, nativeCallable!.nativeFunction);
+    setCallback(engineId, nativeCallable.nativeFunction);
   } catch (e) {
     debugPrint('Failed to set callback: $e');
   }
